b6c4c57683171e92b0f3f1a26f9379e83e657b9e
inesmassa <inesurvivor@gmail.com>
2026-03-09 15:20:56 +0100

    code dans contraintes

--- contraintes.md
+++ contraintes.md
@@ -11,23 +11,47 @@
 - Une seule date de naissance (unique)
 - Une seule date de mort (optionnelle)
 - Sa date de naissance est toujours inférieure à sa date de décès
+```sql
+ADD CONSTRAINT verif_date CHECK (DateNaissance < DateDeces OR DateDeces IS NULL);
+```
+- Ne peut pas s'unir avec lui-même
+```sql
+ADD CONSTRAINT verif_union CHECK (id_individu_a <> id_individu_b),
+```
 
 ## Un enfant :
 
 - Ne peut pas naître avant un parent biologique/adoptif
 - Ne peut pas être son propre enfant
+```sql
+ADD CONSTRAINT verif_enfant_bio CHECK (id_parents <> id_enfant),
+ADD CONSTRAINT verif_enfant_ado CHECK (id_parents <> id_enfant),
+```
 - Peut mourir avant un parent
 
 ## Les parents :
 - Peuvent apparemment être frère et sœur si l'un des deux a été adopté ?...
-- Ne peuvent pas être leurs propres parents
+- Ne peuvent pas être leurs propres parents/beaux-parents
+```sql
+ADD CONSTRAINT verif_beaux_parents CHECK (id_parents <> id_enfant),
+```
 - Peuvent mourir avant ou après son enfant
 - Ne peuvent pas être l'ancêtre de lui-même
 
 ## Union : 
 - Peuvent avoir une seule union à la fois
 - Une union peut avoir une date de début et de fin (optionnelle)
+```sql
+date_fin IS NULL
+```
 - Une autre union peut commencer uniquement si la précédente est terminée
+```sql
+ADD CONSTRAINT verif_date_union CHECK (date_debut < date_fin OR date_fin IS NULL),
+```
+- Une fois une union terminée elle ne peut plus renaitre
+```sql 
+ADD CONSTRAINT unique_union UNIQUE (id_individu_a, id_individu_b);
+```
 
 ## Listes des différentes tables :
 - individu
--- init.sql
+++ init.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE individus_louches;
+-- base de données
+CREATE DATABASE etrange_genealogie;
 -- table individu
 
 CREATE TABLE IF NOT EXISTS individu (
@@ -10,23 +11,26 @@ CREATE TABLE IF NOT EXISTS individu (
 -- contraintes individu
 
 ALTER TABLE individu
-ADD CONSTRAINT verif_date CHECK (DateNaissance < DateDeces) AND DateDeces IS NULL;
+ADD CONSTRAINT verif_date CHECK (DateNaissance < DateDeces OR DateDeces IS NULL);
 
 -- table individu_nom
 
 CREATE TABLE IF NOT EXISTS individu_nom (
     id INT PRIMARY KEY,
     id_individu INT NOT NULL REFERENCES individu(id) ON DELETE CASCADE,
-    nom varchar(100) NOT NULL
+    nom varchar(100) NOT NULL,
+    ordre INT NOT NULL DEFAULT 1
     );
+
 -- table individu_prenom
 
 CREATE TABLE IF NOT EXISTS individu_prenom (
     id INT PRIMARY KEY,
     id_individu INT NOT NULL REFERENCES individu(id) ON DELETE CASCADE,
-    prenom varchar(100) NOT NULL
+    prenom varchar(100) NOT NULL,
+    ordre INT NOT NULL DEFAULT 1
     );
- 
+
 -- table relation_biologique
 
 CREATE TABLE IF NOT EXISTS relation_biologique (
@@ -37,21 +41,21 @@ CREATE TABLE IF NOT EXISTS relation_biologique (
 -- contraintes relation_biologique
 
 ALTER TABLE relation_biologique
-ADD CONSTRAINT verif_enfant_bio CHECK (id_enfant <> id_enfant)
-AND unique_parents_bio UNIQUE (id_parents, id_enfant);
+ADD CONSTRAINT verif_enfant_bio CHECK (id_parents <> id_enfant),
+ADD CONSTRAINT unique_parents_bio UNIQUE (id_parents, id_enfant);
 
 -- table relation_adoptive
 
 CREATE TABLE IF NOT EXISTS relation_adoptive (
     id INT PRIMARY KEY,
-    id_parents INT NOT NULL REFERENCES individu(id) ON DELETE RESTRICT
+    id_parents INT NOT NULL REFERENCES individu(id) ON DELETE RESTRICT,
     id_enfant INT NOT NULL REFERENCES individu(id) ON DELETE RESTRICT
     );
 -- contraintes relation_adoptive
 
 ALTER TABLE relation_adoptive
-ADD CONSTRAINT verif_enfant_ado CHECK (id_enfant <> id_enfant)
-AND unique_parents_ado UNIQUE (id_parents, id_enfant);
+ADD CONSTRAINT verif_enfant_ado CHECK (id_parents <> id_enfant),
+ADD CONSTRAINT unique_parents_ado UNIQUE (id_parents, id_enfant);
 
 -- table relation_beaux_parents
 
@@ -64,8 +68,8 @@ CREATE TABLE IF NOT EXISTS relation_beaux_parents (
 -- contraintes relation_beaux_parents
 
 ALTER TABLE relation_beaux_parents
-ADD CONSTRAINT verif_beaux_parents CHECK (id_parents <> id_enfant)
-AND unique_beaux_parents UNIQUE (id_parents, id_enfant);
+ADD CONSTRAINT verif_beaux_parents CHECK (id_parents <> id_enfant),
+ADD CONSTRAINT unique_beaux_parents UNIQUE (id_parents, id_enfant);
 
 -- table union_conjugale
 
@@ -73,12 +77,12 @@ CREATE TABLE IF NOT EXISTS union_conjugale (
     id INT PRIMARY KEY,
     id_individu_a INT NOT NULL REFERENCES individu(id) ON DELETE RESTRICT,
     id_individu_b INT NOT NULL REFERENCES individu(id) ON DELETE RESTRICT,
-    date_debut DATE NULL
+    date_debut DATE NULL,
     date_fin DATE NULL
     );
 -- contraintes union_conjugale
 
 ALTER TABLE union_conjugale
-ADD CONSTRAINT verif_date_union CHECK (date_debut < date_fin) AND date_fin IS NULL
-AND verif_union CHECK (id_individu_a <> id_individu_b)
-AND unique_union UNIQUE (id_individu_a, id_individu_b);
+ADD CONSTRAINT verif_date_union CHECK (date_debut < date_fin OR date_fin IS NULL),
+ADD CONSTRAINT verif_union CHECK (id_individu_a <> id_individu_b),
+ADD CONSTRAINT unique_union UNIQUE (id_individu_a, id_individu_b);
