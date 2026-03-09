8f120ee94cbbfc2968a546280dc6738e0e400ca9
inesmassa <inesurvivor@gmail.com>
2026-03-09 13:23:04 +0100

    debut creation de toutes les tables

--- init.sql
+++ init.sql
@@ -1,14 +1,49 @@
 CREATE DATABASE individus_louches;
 -- table individu
 
-CREATE TABLE IF NOT EXISTS `individu` (
-  `NumIndividu` int(11) NOT NULL DEFAULT '0',
-  `Prenom` varchar(30) NOT NULL DEFAULT,
-  `Prenom2` varchar(30) DEFAULT NULL,
-  `Nom` varchar(30) NOT NULL DEFAULT,
-  `DateNaissance` DATE,
-  PRIMARY KEY (`NumIndividu`)
+CREATE TABLE IF NOT EXISTS individu (
+    id PRIMARY KEY,
+    DateNaissance DATE NOT NULL,
+    DateDeces DATE DEFAULT NULL
+    );
 
-ALTER TABLE `individu`
-  ADD CONSTRAINT `habitant_ibfk_1` FOREIGN KEY (`NumQualite`) REFERENCES `qualite` (`NumQualite`),
-  ADD CONSTRAINT `habitant_ibfk_2` FOREIGN KEY (`NumVillage`) REFERENCES `village` (`NumVillage`);
+ALTER TABLE individu
+ADD CONSTRAINT verif_date CHECK (DateNaissance < DateDeces);
+
+CREATE TABLE IF NOT EXISTS individu_nom (
+    id PRIMARY KEY,
+    id_individu
+    nom varchar(30) NOT NULL DEFAULT,
+    );
+
+CREATE TABLE IF NOT EXISTS individu_prenom (
+    id PRIMARY KEY,
+    id_individu
+    prenom varchar(30) NOT NULL DEFAULT,
+    );
+
+CREATE TABLE IF NOT EXISTS relation_biologique (
+    id PRIMARY KEY,
+    id_parents
+    id_enfant,
+    );
+
+CREATE TABLE IF NOT EXISTS relation_adoptive (
+    id PRIMARY KEY,
+    id_parents,
+    id_enfant,
+    );
+
+CREATE TABLE IF NOT EXISTS relation_beaux_parents (
+    id PRIMARY KEY,
+    id_parents
+    id_enfant,
+    );
+
+CREATE TABLE IF NOT EXISTS union_conjugale (
+    id PRIMARY KEY,
+    id_individu_a,
+    id_individu_b
+    date_debut DATE DEFAULT NULL
+    date_fin DATE DEFAULT NULL
+    );
